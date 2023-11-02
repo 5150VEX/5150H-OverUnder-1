a255a02ab66c1e64f505025e7528d34c6123c4cf
axo1otl <mattsekiguchi@gmail.com>
2023-11-02 16:12:56 -0400

    added intake code

--- hbor/src/main.cpp
+++ hbor/src/main.cpp
@@ -11,7 +11,7 @@
 pros::Controller controller(pros::E_CONTROLLER_MASTER); // controller setup
 
 pros::Motor cata(20, pros::E_MOTOR_GEARSET_36, false); // cata motor
-pros::Motor intake(2, pros::E_MOTOR_GEARSET_36, false); // intake motor
+pros::Motor intakeMotor(2, pros::E_MOTOR_GEARSET_36, false); // intake motor
 
 // drivebase setup
 pros::Motor lFmotor(14, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_DEGREES);
@@ -102,12 +102,25 @@ void cataLaunch(){
 
 	if (r2 && r1){
 		cata.move(-64);
-	} else {
-		if (r2){
+	} else if (r2){
 			cata.move(-127);
-		} else {
-			cata.brake();
-		}
+	} else {
+		cata.brake();
+	}
+};
+
+void intake(){
+	bool bUp = controller.get_digital(pros::E_CONTROLLER_DIGITAL_UP);
+	bool bDown = controller.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN);
+    
+    int intakeSpeed = 64;
+
+	if (bUp){ // intake
+		intakeMotor.move(-intakeSpeed);
+	} else if (bDown) { // outake
+		intakeMotor.move(intakeSpeed);
+    } else {
+		intakeMotor.brake();
 	}
 };
 
@@ -174,36 +187,12 @@ void competition_initialize() {
 
 }
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
 void autonomous() {
 	chassis.turnTo(30, 0, 5000, false);
 
 	//turnTo(180, 127, 500);
 }
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
 void opcontrol() {
 
 	while (true) {
@@ -222,10 +211,7 @@ void opcontrol() {
 
 		cataLaunch();
 		wingControl();
-
-		
-
-
+		intake();
 
         pros::delay(5);
     }
